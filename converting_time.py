9df428107fe6244642257e7f478ae21a999a4f99
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-08-20 15:32:01 +0300

    function for converting 12-hour time to 24-hour time done

---
+++ converting_time.py
@@ -0,0 +1,9 @@
+def convert_time(hour,minutes,period):
+    if period == "am" and hour==12 :
+        hour=0
+    elif period =="pm" and hour<12 :
+        hour+=12
+    print(f"{hour:02d}{minutes}")
+    return f"{hour:02d}{minutes}"
+convert_time(12,45,"pm")
+
